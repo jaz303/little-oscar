96678e548398965eaaac1933651fe7e1c9071da5
Jason Frame <jason@onehackoranother.com>
2011-11-09 22:33:35 +0000

    tabs -> spaces

--- osc.c
+++ osc.c
@@ -1,7 +1,7 @@
 #include "osc.h"
 
-#define osc_hton32(i)	(htobe32(*((uint32_t*)(&i))))
-#define osc_hton64(i)	(htobe64(*((uint64_t*)(&i))))
+#define osc_hton32(i)   (htobe32(*((uint32_t*)(&i))))
+#define osc_hton64(i)   (htobe64(*((uint64_t*)(&i))))
 
 #ifndef NULL
 #define NULL 0
@@ -10,189 +10,189 @@
 #include <stdio.h>
 
 #define WRITE_FIXED(type, va_type, bits) \
-	if (end - pos < sizeof(type)) return -1; \
-	type real_val = (type) va_arg(args, va_type); \
-	uint##bits##_t bytes = osc_hton##bits(real_val); \
-	memcpy(pos, &bytes, sizeof(type)); \
-	pos += sizeof(type);
+    if (end - pos < sizeof(type)) return -1; \
+    type real_val = (type) va_arg(args, va_type); \
+    uint##bits##_t bytes = osc_hton##bits(real_val); \
+    memcpy(pos, &bytes, sizeof(type)); \
+    pos += sizeof(type);
 
 static inline char* osc_write_aligned(char *buffer, size_t remain, const void *source, size_t len, size_t already_written) {
-	size_t unrounded	= len + already_written;
-	size_t rounded		= (unrounded + 3) & ~0x03;
-	
-	if (rounded - already_written > remain) return NULL;
-	
-	memcpy(buffer, source, len);
-	buffer += len;
-	switch (rounded - unrounded) {
-		case 3: *(buffer++) = 0;
-		case 2: *(buffer++) = 0;
-		case 1: *(buffer++) = 0;
-		case 0: break;
-	}
-	
-	return buffer;
+    size_t unrounded    = len + already_written;
+    size_t rounded      = (unrounded + 3) & ~0x03;
+    
+    if (rounded - already_written > remain) return NULL;
+    
+    memcpy(buffer, source, len);
+    buffer += len;
+    switch (rounded - unrounded) {
+        case 3: *(buffer++) = 0;
+        case 2: *(buffer++) = 0;
+        case 1: *(buffer++) = 0;
+        case 0: break;
+    }
+    
+    return buffer;
 }
 
 int osc_writev(char *buffer, size_t buffer_len, const char *address, const char *format, va_list args) {
-	
-	char *pos	= buffer,
-		 *end	= buffer + buffer_len;
-		 
-	pos = osc_write_aligned(pos, end - pos, address, strlen(address) + 1, 0);
-	if (!pos) return -1;
-	
-	if (end - pos < 1) return -1;
-	
-	*(pos++) = ',';
-	pos = osc_write_aligned(pos, end - pos, format, strlen(format) + 1, 1);
-	if (!pos) return -1;
-	
-	const char *curr = format;
-	while (*curr) {
-		/*
-		 * Not implemented:
-		 * c  - 32-bit ASCII char
-		 * r  - 32-bit RGBA color
-		 * m  - 4-byte MIDI message
-		 * [] - arrays
-		 */
-		switch (*curr) {
-			case 'T':
-			case 'F':
-			case 'N':
-			case 'I':
-				/* true, false, nil and infinitum allocate no bytes in argument data */
-				break;
-			
-			case 'i': { WRITE_FIXED(int32_t, int, 32);			break; /* int32 */ }
-			case 'h': { WRITE_FIXED(int64_t, int64_t, 64);		break; /* 64-bit big-endian signed integer */ }
-			case 't': { WRITE_FIXED(uint64_t, uint64_t, 64);	break; /* OSC-timetag */ }
-			case 'f': { WRITE_FIXED(float, double, 32);			break; /* float32 */ }
-			case 'd': { WRITE_FIXED(double, double, 64);		break; /* double */ }
-			case 's': /* OSC-string */
-			case 'S': /* alternate string */
-			{
-				const char *string_val = va_arg(args, char*);
-				pos = osc_write_aligned(pos, end - pos, string_val, strlen(string_val) + 1, 0);
-				if (!pos) return -1;
-				break;
-			}
-			case 'b': /* OSC-blob */
-			{
-				WRITE_FIXED(int32_t, int, 32); /* write the blob length; the value is stored in `real_val` */
-				char *blob_ptr = va_arg(args, char*);
-				pos = osc_write_aligned(pos, end - pos, blob_ptr, real_val, 0);
-				if (!pos) return -1;
-				break;
-			}
-			default:
-				return -1;
-		}
-		curr++;
-	}
-		
-	return (int) (pos - buffer);
-	
+    
+    char *pos   = buffer,
+         *end   = buffer + buffer_len;
+         
+    pos = osc_write_aligned(pos, end - pos, address, strlen(address) + 1, 0);
+    if (!pos) return -1;
+    
+    if (end - pos < 1) return -1;
+    
+    *(pos++) = ',';
+    pos = osc_write_aligned(pos, end - pos, format, strlen(format) + 1, 1);
+    if (!pos) return -1;
+    
+    const char *curr = format;
+    while (*curr) {
+        /*
+         * Not implemented:
+         * c  - 32-bit ASCII char
+         * r  - 32-bit RGBA color
+         * m  - 4-byte MIDI message
+         * [] - arrays
+         */
+        switch (*curr) {
+            case 'T':
+            case 'F':
+            case 'N':
+            case 'I':
+                /* true, false, nil and infinitum allocate no bytes in argument data */
+                break;
+            
+            case 'i': { WRITE_FIXED(int32_t, int, 32);          break; /* int32 */ }
+            case 'h': { WRITE_FIXED(int64_t, int64_t, 64);      break; /* 64-bit big-endian signed integer */ }
+            case 't': { WRITE_FIXED(uint64_t, uint64_t, 64);    break; /* OSC-timetag */ }
+            case 'f': { WRITE_FIXED(float, double, 32);         break; /* float32 */ }
+            case 'd': { WRITE_FIXED(double, double, 64);        break; /* double */ }
+            case 's': /* OSC-string */
+            case 'S': /* alternate string */
+            {
+                const char *string_val = va_arg(args, char*);
+                pos = osc_write_aligned(pos, end - pos, string_val, strlen(string_val) + 1, 0);
+                if (!pos) return -1;
+                break;
+            }
+            case 'b': /* OSC-blob */
+            {
+                WRITE_FIXED(int32_t, int, 32); /* write the blob length; the value is stored in `real_val` */
+                char *blob_ptr = va_arg(args, char*);
+                pos = osc_write_aligned(pos, end - pos, blob_ptr, real_val, 0);
+                if (!pos) return -1;
+                break;
+            }
+            default:
+                return -1;
+        }
+        curr++;
+    }
+        
+    return (int) (pos - buffer);
+    
 }
 
 int osc_write(char *buffer, size_t buffer_len, const char *address, const char *format, ...) {
-	va_list args;
-	va_start(args, format);
-	int written = osc_writev(buffer, buffer_len, address, format, args);
-	va_end(args);
-	
-	return written;
+    va_list args;
+    va_start(args, format);
+    int written = osc_writev(buffer, buffer_len, address, format, args);
+    va_end(args);
+    
+    return written;
 }
 
 int osc_bundle_init(osc_bundle_t *bundle, char *buffer, size_t buffer_sz) {
-	bundle->buffer		= buffer;
-	bundle->buffer_pos	= buffer;
-	bundle->buffer_end	= buffer + buffer_sz;
-	bundle->len			= 0;
+    bundle->buffer      = buffer;
+    bundle->buffer_pos  = buffer;
+    bundle->buffer_end  = buffer + buffer_sz;
+    bundle->len         = 0;
 }
 
 int osc_bundle_start(osc_bundle_t *bundle, osc_timetag_t when) {
-	bundle->buffer_pos	= bundle->buffer;
-	bundle->len			= 0;
-	
-	bundle->buffer_pos	= osc_write_aligned(bundle->buffer_pos,
-										    bundle->buffer_end - bundle->buffer_pos,
-										    "#bundle",
-										    8,
-										    0);
-	
-	if (!bundle->buffer_pos) return 0;
-	if (bundle->buffer_end - bundle->buffer_pos < 8) return 0;
-	
-	uint64_t when_nbo = osc_hton64(when);
-	memcpy(bundle->buffer_pos, &when_nbo, 8);
-	bundle->buffer_pos += 8;
-	
-	bundle->len = bundle->buffer_pos - bundle->buffer;
-	
-	return 1;
+    bundle->buffer_pos  = bundle->buffer;
+    bundle->len         = 0;
+    
+    bundle->buffer_pos  = osc_write_aligned(bundle->buffer_pos,
+                                            bundle->buffer_end - bundle->buffer_pos,
+                                            "#bundle",
+                                            8,
+                                            0);
+    
+    if (!bundle->buffer_pos) return 0;
+    if (bundle->buffer_end - bundle->buffer_pos < 8) return 0;
+    
+    uint64_t when_nbo = osc_hton64(when);
+    memcpy(bundle->buffer_pos, &when_nbo, 8);
+    bundle->buffer_pos += 8;
+    
+    bundle->len = bundle->buffer_pos - bundle->buffer;
+    
+    return 1;
 }
 
 int osc_bundle_writev(osc_bundle_t *bundle, const char *address, const char *format, va_list args) {
-	if (bundle->buffer_end - bundle->buffer_pos < 4) return 0;
-	
-	char *buffer_before = bundle->buffer_pos;
-	bundle->buffer_pos += 4;
-	
-	int written = osc_writev(bundle->buffer_pos,
-							 bundle->buffer_end - bundle->buffer_pos,
-							 address,
-							 format,
-							 args);
-	
-	if (written < 0) return 0;
-	
-	bundle->buffer_pos += written;
-	
-	uint32_t written_nbo = osc_hton32(written);
-	memcpy(buffer_before, &written_nbo, 4);
-	
-	bundle->len += written + 4;
-	
-	return 1;
+    if (bundle->buffer_end - bundle->buffer_pos < 4) return 0;
+    
+    char *buffer_before = bundle->buffer_pos;
+    bundle->buffer_pos += 4;
+    
+    int written = osc_writev(bundle->buffer_pos,
+                             bundle->buffer_end - bundle->buffer_pos,
+                             address,
+                             format,
+                             args);
+    
+    if (written < 0) return 0;
+    
+    bundle->buffer_pos += written;
+    
+    uint32_t written_nbo = osc_hton32(written);
+    memcpy(buffer_before, &written_nbo, 4);
+    
+    bundle->len += written + 4;
+    
+    return 1;
 }
 
 int osc_bundle_write(osc_bundle_t *bundle, const char *address, const char *format, ...) {
-	va_list args;
-	va_start(args, format);
-	int retval = osc_bundle_writev(bundle, address, format, args);
-	va_end(args);
-	
-	return retval;
+    va_list args;
+    va_start(args, format);
+    int retval = osc_bundle_writev(bundle, address, format, args);
+    va_end(args);
+    
+    return retval;
 }
 
 int tuio_write_2Dobj(char *buffer, size_t buffer_len, struct tuio_msg_2d *msg) {
-	return osc_write(buffer, buffer_len, "/tuio/2Dobj",
-										 "siiffffffff", "set",
-			   											msg->session_id,
-			   											msg->class_id,
-			   											msg->x, msg->y, msg->a,
-			   											msg->vx, msg->vy, msg->va,
-			   											msg->motion_acceleration,
-			   											msg->rotation_acceleration);
+    return osc_write(buffer, buffer_len, "/tuio/2Dobj",
+                                         "siiffffffff", "set",
+                                                        msg->session_id,
+                                                        msg->class_id,
+                                                        msg->x, msg->y, msg->a,
+                                                        msg->vx, msg->vy, msg->va,
+                                                        msg->motion_acceleration,
+                                                        msg->rotation_acceleration);
 }
 
 int tuio_write_2Dcur(char *buffer, size_t buffer_len, struct tuio_msg_2d *msg) {
-	return osc_write(buffer, buffer_len, "/tuio/2Dcur",
-										 "sifffff", "set",
-													msg->session_id,
-													msg->x, msg->y,
-													msg->vx, msg->vy,
-													msg->motion_acceleration);
+    return osc_write(buffer, buffer_len, "/tuio/2Dcur",
+                                         "sifffff", "set",
+                                                    msg->session_id,
+                                                    msg->x, msg->y,
+                                                    msg->vx, msg->vy,
+                                                    msg->motion_acceleration);
 }
 
 int tuio_write_2Dblb(char *buffer, size_t buffer_len, struct tuio_msg_2d *msg) {
-	return osc_write(buffer, buffer_len, "/tuio/2Dblb",
-										 "sfffffffffff", "set",
-														 msg->x, msg->y, msg->a,
-														 msg->width, msg->height, msg->area,
-														 msg->vx, msg->vy, msg->va,
-														 msg->motion_acceleration,
-														 msg->rotation_acceleration);
+    return osc_write(buffer, buffer_len, "/tuio/2Dblb",
+                                         "sfffffffffff", "set",
+                                                         msg->x, msg->y, msg->a,
+                                                         msg->width, msg->height, msg->area,
+                                                         msg->vx, msg->vy, msg->va,
+                                                         msg->motion_acceleration,
+                                                         msg->rotation_acceleration);
 }
--- test.c
+++ test.c
@@ -12,41 +12,41 @@
 #define BUFFER_LEN 1536
 
 int main(int argc, char *argv[]) {
-	
-	int sock = socket(AF_INET, SOCK_DGRAM, 0);
-	int port = 9000;
-	
-	struct sockaddr_in addr;
-	struct hostent *hp = gethostbyname("127.0.0.1");
+    
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    int port = 9000;
+    
+    struct sockaddr_in addr;
+    struct hostent *hp = gethostbyname("127.0.0.1");
   
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	memcpy(&addr.sin_addr, hp->h_addr, hp->h_length);
-	addr.sin_port = htons(port);
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    memcpy(&addr.sin_addr, hp->h_addr, hp->h_length);
+    addr.sin_port = htons(port);
 
-	char buffer[BUFFER_LEN];
-	osc_bundle_t bundle;
-	
-	osc_bundle_init(&bundle, buffer, BUFFER_LEN);
-	
-	int i = 0;
-	float f = 0.0;
-	
-	while (1) {
-		osc_bundle_start(&bundle, OSC_NOW);
-		osc_bundle_write(&bundle, "/foo/bar", "iifs", i, 2, f, "string string");
-		osc_bundle_write(&bundle, "/baz/bleem", "ifs", i * 2, f / 2, "another string string");
-		osc_bundle_write(&bundle, "/boof/barf", "iii", i * 3, i * 4, i * 5);
-		
-		int written = sendto(sock, bundle.buffer, bundle.len, 0, (struct sockaddr *)&addr, sizeof(addr));
-		printf("sent %d bytes...\n", written);
-		i += 1;
-		f += 0.05;
-		sleep(1);
-	}
-	
-	return 0;	
-	
+    char buffer[BUFFER_LEN];
+    osc_bundle_t bundle;
+    
+    osc_bundle_init(&bundle, buffer, BUFFER_LEN);
+    
+    int i = 0;
+    float f = 0.0;
+    
+    while (1) {
+        osc_bundle_start(&bundle, OSC_NOW);
+        osc_bundle_write(&bundle, "/foo/bar", "iifs", i, 2, f, "string string");
+        osc_bundle_write(&bundle, "/baz/bleem", "ifs", i * 2, f / 2, "another string string");
+        osc_bundle_write(&bundle, "/boof/barf", "iii", i * 3, i * 4, i * 5);
+        
+        int written = sendto(sock, bundle.buffer, bundle.len, 0, (struct sockaddr *)&addr, sizeof(addr));
+        printf("sent %d bytes...\n", written);
+        i += 1;
+        f += 0.05;
+        sleep(1);
+    }
+    
+    return 0;   
+    
 }
 
 
